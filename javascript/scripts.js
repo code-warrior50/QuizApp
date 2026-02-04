b2f2e43e6ed0f9456fa6c364480662a6bceac725
Keith Thomas <6kthomas@gmail.com>
2026-02-03 22:14:15 -0600

    Added some javascript

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,32 @@
+const answerOptions = document.querySelector(".answeroptions");
+
+let quizCategory = "programming";
+
+// Fetch a random question from based on the selected category
 const getRandomQuestion = () => {
-    const categoryQuestions =
+    const categoryQuestions = questions.find(cat => cat.category.toLowerCase() === quizCategory.toLowerCase()).questions || [];
+    
+    const randomQuestion = categoryQuestions[Math.floor(Math.random() * categoryQuestions.length)];
+    return randomQuestion;
 }
+
+// Render the current question and its options in the quiz
+const renderQuestion = () => {
+    const currentQuestion = getRandomQuestion();
+    if(!currentQuestion) return;
+
+    // Update the UI
+    answerOptions.innerHTML = "";
+    document.querySelector(".question-text").textContent = currentQuestion.question;
+
+    // Create option <li> elements and append them
+
+    currentQuestion.options.forEach(option => {
+        const li = document.createElement("li");
+        li.classList.add("answer-option");
+        li.textContent = option;
+        answerOptions.appendChild(li);
+    });
+}
+
+renderQuestion();
